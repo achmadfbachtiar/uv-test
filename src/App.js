27053a30cdf26237ea038f316ee32cefe3c64a6c
Achmad Fauzi Bachtiar <achmadfauzibachtiar@gmail.com>
2019-05-21 16:13:42 +0700

    cats

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      images: []
+    };
+  }
+
+  componentWillMount(){
+    this.getImages("1")
+  }
+
+  async getImages(categoryId) {
+    try {
+      let response = await fetch(
+        'https://api.thecatapi.com/v1/images/search?category_ids='+categoryId+'&limit=50', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'x-api-key': 'ABC123',
+        }
+      });
+      let responseJson = await response.json();
+      console.log(responseJson[0].url);
+      this.setState({
+        images: responseJson
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  card(data){
+    return(
+      <div className="card" style={{background: 'url('+data.url+')'}}></div>
+    )
+  }
+
+  render() {
+    return(
+      <div className="flex" style={{display: 'flex'}}>
+        <div className="flex sidebar">
+          <p>Category</p>
+          <p className="category" onClick={() => this.getImages("1")}>
+            Hats
+          </p>
+          <p className="category" onClick={() => this.getImages("5")}>
+            Boxes
+          </p>
+          <p className="category" onClick={() => this.getImages("7")}>
+            Ties
+          </p>
+        </div>
+        <div className="flex content-wrapper">
+          {this.state.images.map((data, index) => {
+            return(
+              this.card(data)
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
